07dd46e61473dd2e0c48492b6a14839089badca9
PeterB22 <peter.borcsok22@gmail.com>
2017-02-17 15:51:32 +0100

    solving argument passing

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {FilterComponent} from '../filter/filter.component'
+import {Input} from '@angular/core'
+import {Country} from '../model/country.model'
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  inputs:['countries'],
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
+  directives: [FilterComponent]
 })
 export class EditComponent implements OnInit {
-
+  @Input country : Country;
   constructor() { }
   
 
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -23,31 +23,42 @@ export class FilterComponent implements OnInit {
   getDataByValue(type,value){
      if(value !=""){
        this.countries = [];
-      this.http.get("https://restcountries.eu/rest/v1/" + type + "/" + value)
-               .subscribe(res=>{
-                 res.json();
-                 for(var i=0;i<res.json().length;i++){
-                   var country :Country = new CountryBuilder(res.json()[i].name)
-                                       .setCapital(res.json()[i].capital)
-                                       .setLanguage(res.json()[i].languages)
-                                       .setPopulation(res.json()[i].population)
-                                       .setRegion(res.json()[i].region)
-                                       .setSubRegion(res.json()[i].subregion)
-                                       .setTopLevelDomain(res.json()[i].topLevelDomain)
-                                       .setCallingCodes(res.json()[i].callingCodes)
-                                       .build();
+       this.http.get("https://restcountries.eu/rest/v1/" + type + "/" + value)
+                .subscribe(res=>{
+                   res.json();
+                   for(var i=0;i<res.json().length;i++){
+                     var country :Country = new CountryBuilder(res.json()[i].name)
+                                         .setCapital(res.json()[i].capital)
+                                         .setLanguage(res.json()[i].languages)
+                                         .setPopulation(res.json()[i].population)
+                                         .setRegion(res.json()[i].region)
+                                         .setSubRegion(res.json()[i].subregion)
+                                         .setTopLevelDomain(res.json()[i].topLevelDomain)
+                                         .setCallingCodes(res.json()[i].callingCodes)
+                                         .build();
+                     
+                     this.countries.push(country);                   
+                   }
                    
-                   this.countries.push(country);                   
-                 }
-                 
-                }
-               // error=>console.log("error")
-               );
-   
+                  }
+                 // error=>console.log("error")
+                 );
+     
       
   
     }
    // console.log(this.countries);
+  }
+
+
+
+  selectCountry(index){
+      var nodes = document.getElementsByTagName("app-filter");
+
+      for (var i = 0, len = nodes.length; i != len; ++i) 
+        nodes[0].parentNode.removeChild(nodes[0]);
+
+      console.log(index);
   }
 
   
