3e3122f2e966ffd679f4fa5f278f0647da840c01
PeterB22 <peter.borcsok22@gmail.com>
2017-02-24 10:53:22 +0100

    added slider functionalities

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,11 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SettingsComponent implements OnInit {
+  value:number;
+  
+  constructor() {
 
-  constructor() { }
+   }
+ 
 
+  onSliderChange(value){
+    window.localStorage.setItem("numOfRecords",value);
+    this.value=value;
+  }
 
   ngOnInit() {
+    if(window.localStorage.getItem("numOfRecords") == null){
+      this.value=10;
+      window.localStorage.setItem("numOfRecords",this.value.toString());
+    }
+    else{
+      this.value=parseInt(window.localStorage.getItem("numOfRecords"));
+    }
     document.getElementById("welcome").style.display="none";
   }
 
